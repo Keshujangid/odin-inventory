1277d682d0aa8f39bc42aefa1279179a78c1588f
keshu <keshujangid191@gmail.com>
2024-09-05 10:25:59 +0530

    md vercel.json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express")
 const app = express();
 const usersRouter = require("./routes/router")
 // const multer = require('multer')
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 
